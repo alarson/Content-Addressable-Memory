179b3dddea5038de09a1bf1da3cec477c201404c
Ari <arielsebastianj@gmail.com>
2016-04-12 14:17:19 -0400

    implemented memory storage and removal

---
+++ demo.py
@@ -0,0 +1,9 @@
+#First attempt at use case of addressable memory using hopfield networks
+#Class page: http://www.cems.uvm.edu/~rsnapp/teaching/cs256/index.html
+#Ari Larson
+#4/12/2016
+import network
+def main():
+	a = network.Hopfield_Network(2)
+	print a
+main()
--- network.py
+++ network.py
@@ -3,29 +3,58 @@
 #Ari Larson
 #4/12/2016
 
+import numpy as np
+
 class Hopfield_Network(object):
 	'''
 	Meant to serve as a model of the hopefield associative memory
 	'''
 
-	def __init__(n,m):
+	def __init__(self, n):
 		'''
 		The network should be initialized with desired dimensions, equal to the dimensions 
 		of the units stored and retrieved
 		'''
+		self.n = n
+		self.weights = np.matrix([np.zeros(n),np.zeros(n)])
+		
+		self.memories = []
+		self.num_memories = 0
 
-	def store():
-		'''
-		The passed matrix is added to the set of stored matrices, and the network weights are re-calculated.
-		'''
+		#self.state_vector = 
 
-	def remove():
-		'''
-		The passed matrix is removed from the set of stored matrices, and the network weights are re-calculated. If
-		the passed matrix does not exist in the set of stored values, then a warning is raised and no change is made to the network weights.
-		'''
+	def __str__(self):
+		return str(self.weights)
 
-	def retrieve():
+	def store(self, memory):
+		'''
+		The passed memory vector is added to the set of stored matrices, and the network weights are re-calculated.
+		'''
+		self.memories.append(memory)
+		self.num_memories += 1
+		self.sum_outer_products()
+
+	def remove(self, memory):
+		'''
+		The passed memory vector is removed from the set of stored matrices, and the network weights are re-calculated. If
+		the passed vector does not exist in the set of stored values, then a warning is raised and no change is made to the network weights.
+		'''
+		if(memory in self.memories):
+			self.memories.remove(memory)
+			self.num_memories -= 1
+			self.sum_outer_products()
+		else:
+			print "Attempted to remove memory, not present in weight matrix"
+
+	def sum_outer_products(self):
+		if(self.memories): #nonempty
+			self.weights = np.zeros((self.n,self.n))
+			for memory in self.memories:
+				outer_product = memory*np.transpose(memory) - np.identity(self.n)
+				self.weights+=outer_product
+		else:
+			print "attempted to sum outer products of unpopulated memory set"
+	def retrieve(self, probe):
 		'''
 		The passed matrix is used to initialize the state of the network, and should be a mildly permuted version of the memory
 		that should be retrieved. This function then starts the update() cycle, and ends when the network reaches a stable state.
@@ -33,12 +62,12 @@ class Hopfield_Network(object):
 		Dr. Snapp in the class notes: http://www.cems.uvm.edu/~rsnapp/teaching/cs256/index.html
 		'''
 
-	def update_asynch():
+	def update_asynch(self):
 		'''
 		One timestep in the network update process, when the network is updated asynchronously
 		'''
 
-	def update_synch():
+	def update_synch(self):
 		'''
 		One timestep in the network update process, when the network is updated synchronously
 		'''
