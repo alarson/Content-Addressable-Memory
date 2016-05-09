ea7d4fe4dced1772f22297028adafc38008b207f
Ari Larson <arielsebastianj@gmail.com>
2016-05-09 09:44:36 -0400

    made some heatmaps

--- python/heatmap.py
+++ python/heatmap.py
@@ -10,26 +10,33 @@ import math
 
 def main(argv):
 	filename = argv[0]+'.pickle'
+	filename2 = argv[1]+'.pickle'
 	with open(filename, 'rb') as handle:
 		to_graph = pickle.load(handle)
 
+	with open(filename2, 'rb') as handle:
+		second = pickle.load(handle)
+
 	to_graph.reverse()
+	second.reverse()
 	fig, ax = plt.subplots()
 
 		#and graph the expected relationship
-	x = ([n for n in range(2,2000)])  
+	x = ([n for n in range(2,225)])  
 	y = []
 	for i in x:
 		y.append(meliece_expected(i))
 	ax.plot(x, y)  
 	print x
 	print y
-
+	for i in range(len(to_graph)):
+		for j in range(len(to_graph[i])):
+			to_graph[i][j]+=second[i][j]
 
 	plt.title("Partition Location over time, E5",fontsize=20)
 	
 	
-	plt.imshow(to_graph, cmap=plt.cm.gray,interpolation='nearest', extent=[0,25,0,125], aspect = 25/225.0)
+	# plt.imshow(to_graph, cmap=plt.cm.gray,interpolation='nearest', extent=[0,25,0,125], aspect = 25/225.0)
 
 	# We need to draw the canvas, otherwise the labels won't be positioned and 
 	# won't have values yet.
@@ -38,10 +45,12 @@ def main(argv):
 	plt.xlabel('n')
 	plt.title('r in vanilla networks')
 	ax.set_yticklabels([n for n in range(0,30,4)])
-	ax.set_xticklabels([m for m in range(0,250,25)])
+	ax.set_xticklabels([m for m in range(0,250,50)])
 
 
-	plt.show()
+	# plt.show()
+	plt.savefig('r_perf_25.png')
+
 
 def meliece_expected(x):
 	return x/(4*math.log(x))
